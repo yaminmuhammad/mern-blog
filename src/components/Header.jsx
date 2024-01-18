bde46b17e846f5a2f4e1247cdcc9591f350d5259
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-01-18 20:03:19 +0700

    add: routing with react router 6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../images/logo.png";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<nav>
+			<div className="container nav__container">
+				<Link to="/" className="nav__logo">
+					<img src={Logo} alt="logo" width={60} />
+				</Link>
+				<ul>
+					<li>
+						<Link to="/profile"></Link>
+					</li>
+					<li>
+						<Link to="/create"></Link>
+					</li>
+					<li>
+						<Link to="/authors"></Link>
+					</li>
+					<li>
+						<Link to="/logout"></Link>
+					</li>
+				</ul>
+				<button className="nav__toggle-btn">
+					<AiOutlineClose />
+				</button>
+			</div>
+		</nav>
+	);
 };
 
 export default Header;
