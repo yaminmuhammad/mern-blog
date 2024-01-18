12d2260d6be2e01a06aaa90a42a0f636096290fd
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-01-18 19:16:30 +0700

    add: component layout header footer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import "./index.css";
+import Layout from "./components/Layout";
+import ErrorPage from "./pages/ErrorPage";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [{ index: true, element: <Home /> }],
+	},
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<h1>Hello bang</h1>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
