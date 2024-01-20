c193b075b8bc2de7219a0369dfd9be12ad3fdf23
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-01-20 11:11:23 +0700

    add: Author & category post

--- src/pages/AuthorPosts.jsx
+++ src/pages/AuthorPosts.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import DUMMY_POSTS from "../data";
+import PostItem from "../components/PostItem";
 
 const AuthorPosts = () => {
-	return <div>AuthorPosts</div>;
+	const [posts, setPosts] = useState(DUMMY_POSTS);
+
+	return (
+		<section className="posts">
+			{posts.length > 0 ? (
+				<div className="container posts__container">
+					{posts.map(({ id, thumbnail, category, title, desc, authorID }) => (
+						<PostItem
+							key={id}
+							postID={id}
+							thumbnail={thumbnail}
+							category={category}
+							title={title}
+							desc={desc}
+							authorID={authorID}
+						/>
+					))}
+				</div>
+			) : (
+				<h2 className="center">No posts Found</h2>
+			)}
+		</section>
+	);
 };
 
 export default AuthorPosts;
