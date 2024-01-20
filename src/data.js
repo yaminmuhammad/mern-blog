ae40b1414af3cbb27cf89965a79bee30598b68ff
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-01-20 07:09:21 +0700

    add: Post Detail Page

---
+++ src/data.js
@@ -0,0 +1,41 @@
+import Thumbnail1 from "./images/blog1.jpg";
+import Thumbnail2 from "./images/blog2.jpg";
+import Thumbnail3 from "./images/blog3.jpg";
+import Thumbnail4 from "./images/blog4.jpg";
+
+const DUMMY_POSTS = [
+	{
+		id: "1",
+		thumbnail: Thumbnail1,
+		category: "Agriculture",
+		title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		desc: "lorem ipsum dolorLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit",
+		authorID: 3,
+	},
+	{
+		id: "2",
+		thumbnail: Thumbnail2,
+		category: "Agriculture",
+		title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		desc: "lorem ipsum dolor",
+		authorID: 4,
+	},
+	{
+		id: "3",
+		thumbnail: Thumbnail3,
+		category: "Agriculture",
+		title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		desc: "lorem ipsum dolor",
+		authorID: 2,
+	},
+	{
+		id: "4",
+		thumbnail: Thumbnail4,
+		category: "Agriculture",
+		title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+		desc: "lorem ipsum dolor",
+		authorID: 1,
+	},
+];
+
+export default DUMMY_POSTS;
--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import PostAuthor from "../components/PostAuthor";
+import { Link } from "react-router-dom";
+import Thumbnail from "../images/blog22.jpg";
 
 const PostDetail = () => {
-	return <div>PostDetail</div>;
+	return (
+		<section className="post-detail">
+			<div className="container post-detail__container">
+				<div className="post-detail__header">
+					<PostAuthor />
+					<div className="post-detail__buttons">
+						<Link to={"/posts/werwer/edit"} className="btn sm primary">
+							Edit
+						</Link>
+						<Link to={"/posts/werwer/delete"} className="btn sm danger">
+							Delete
+						</Link>
+					</div>
+				</div>
+				<h1>This is the post title!</h1>
+				<div className="post-detail__thumbnail">
+					<img src={Thumbnail} alt="thumbnail" />
+				</div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+					adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+					amet consectetur adipisicing elit.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+					adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+					amet consectetur adipisicing elit.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+					adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+					amet consectetur adipisicing elit.
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
+					adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
+					amet consectetur adipisicing elit.
+				</p>
+			</div>
+		</section>
+	);
 };
 
 export default PostDetail;
