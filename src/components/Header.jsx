9f98b5078dfb914414b3fa96853d4a224ba34ef8
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-01-21 08:46:17 +0700

    add: style responsive mediaqueries

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../images/logo.png";
 import { FaBars } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 
 const Header = () => {
+	const [isNavShowing, setIsNavShowing] = useState(window.innerWidth > 800 ? true : false);
+
+	const closeNavbarHandler = () => {
+		if (window.innerWidth < 800) {
+			setIsNavShowing(false);
+		} else {
+			setIsNavShowing(true);
+		}
+	};
+
 	return (
 		<nav>
 			<div className="container nav__container">
-				<Link to="/" className="nav__logo">
+				<Link to="/" className="nav__logo" onClick={closeNavbarHandler}>
 					<img src={Logo} alt="logo" />
 				</Link>
-				<ul className="nav__menu">
-					<li>
-						<Link to="/profile/sdfsdf">Makan</Link>
-					</li>
-					<li>
-						<Link to="/create">Minum</Link>
-					</li>
-					<li>
-						<Link to="/authors">About</Link>
-					</li>
-					<li>
-						<Link to="/logout">Career</Link>
-					</li>
-				</ul>
-				<button className="nav__toggle-btn">
-					<AiOutlineClose />
+				{isNavShowing && (
+					<ul className="nav__menu">
+						<li>
+							<Link to="/profile/sdfsdf" onClick={closeNavbarHandler}>
+								Makan
+							</Link>
+						</li>
+						<li>
+							<Link to="/create" onClick={closeNavbarHandler}>
+								Minum
+							</Link>
+						</li>
+						<li>
+							<Link to="/authors" onClick={closeNavbarHandler}>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link to="/logout" onClick={closeNavbarHandler}>
+								Career
+							</Link>
+						</li>
+					</ul>
+				)}
+				<button className="nav__toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+					{isNavShowing ? <AiOutlineClose /> : <FaBars />}
 				</button>
 			</div>
 		</nav>
