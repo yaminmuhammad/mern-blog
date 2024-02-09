2508d33a95c3cdade7086d7b4a985aba2fe1b758
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-02-09 13:55:01 +0700

    fix: register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-
 
 const Register = () => {
 	const [userData, setUserData] = useState({
@@ -10,7 +9,8 @@ const Register = () => {
 		password: "",
 		password2: "",
 	});
-	const [error]
+	const [error, setError] = useState("");
+	const navigate = useNavigate();
 
 	const changeInputHandler = (e) => {
 		setUserData((prevState) => {
@@ -21,12 +21,30 @@ const Register = () => {
 		});
 	};
 
+	const registerUser = async (e) => {
+		e.preventDefault();
+		setError("");
+		try {
+			const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/users/register`, userData);
+
+			const newUser = await response.data;
+			console.log(newUser);
+			if (!newUser) {
+				setError("Couldn't register user. please try again");
+			}
+			navigate("/");
+		} catch (err) {
+			console.error("Axios error:", err);
+			setError(err.response ? err.response.data.message : "An error occurred");
+		}
+	};
+
 	return (
 		<section className="register">
 			<div className="container">
 				<h2>Sign Up</h2>
-				<form className="form register__form">
-					<p className="form__error-message">This is an error message</p>
+				<form className="form register__form" onSubmit={registerUser}>
+					{error && <p className="form__error-message">{error}</p>}
 					<input
 						type="text"
 						placeholder="Full Name"
