1082bf4379b6c4e1e4a4875468d22c447ac0a8d5
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-02-11 06:31:13 +0700

    fix: update user

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -8,11 +8,13 @@ import axios from "axios";
 
 const UserProfile = () => {
 	const [avatar, setAvatar] = useState("");
+	const [previewAvatar, setPreviewAvatar] = useState("");
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [currentPassword, setCurrentPassword] = useState("");
 	const [newPassword, setNewPassword] = useState("");
 	const [confirmNewPassword, setConfirmNewPassword] = useState("");
+	const [error, setError] = useState("");
 
 	const [isAvatarClick, setIsAvatarClick] = useState(false);
 	const navigate = useNavigate();
@@ -20,27 +22,35 @@ const UserProfile = () => {
 	const { currentUser } = useContext(UserContext);
 	const token = currentUser?.token;
 
+	const getUser = async () => {
+		const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/users/${currentUser?.id}`, {
+			withCredentials: true,
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		const { name, email, avatar } = response?.data;
+		setName(name);
+		setEmail(email);
+		setAvatar(avatar);
+		setPreviewAvatar(`${process.env.REACT_APP_ASSETS_URL}/uploads/${avatar}`); // Set preview avatar saat mendapatkan data pengguna
+	};
+
 	useEffect(() => {
 		if (!token) {
 			navigate("/login");
 		}
-	}, []);
+	}, [token, navigate]);
 
 	useEffect(() => {
-		const getUser = async () => {
-			const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/users/${currentUser?.id}`, {
-				withCredentials: true,
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
-			const { name, email, avatar } = response?.data;
-			setName(name);
-			setEmail(email);
-			setAvatar(avatar);
-		};
 		getUser();
-	}, []);
+	}, [token]);
+
+	const handleAvatarChange = (e) => {
+		// Update preview avatar saat memilih gambar
+		setAvatar(e.target.files[0]);
+		setPreviewAvatar(URL.createObjectURL(e.target.files[0]));
+	};
 
 	const changeAvatar = async () => {
 		setIsAvatarClick(false);
@@ -54,8 +64,34 @@ const UserProfile = () => {
 				},
 			});
 			setAvatar(response.data.avatar);
+			getUser();
 		} catch (error) {
 			console.log(error);
+		}
+	};
+
+	const updateUserDetails = async (e) => {
+		e.preventDefault();
+
+		try {
+			const userData = new FormData();
+			userData.set("name", name);
+			userData.set("email", email);
+			userData.set("currentPassword", currentPassword);
+			userData.set("newPassword", newPassword);
+			userData.set("confirmNewPassword", confirmNewPassword);
+
+			const response = await axios.patch(`${process.env.REACT_APP_BASE_URL}/users/edit-user`, userData, {
+				withCredentials: true,
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			if (response.status === 200) {
+				navigate("/logout");
+			}
+		} catch (error) {
+			setError(error.response?.data?.message);
 		}
 	};
 
@@ -68,7 +104,7 @@ const UserProfile = () => {
 				<div className="profile__details">
 					<div className="avatar__wrapper">
 						<div className="profile__avatar">
-							<img src={`${process.env.REACT_APP_ASSETS_URL}/uploads/${avatar}`} alt={""} />
+							<img src={previewAvatar} alt={""} />
 						</div>
 						{/* Form Update Avatar */}
 						<form className="avatar__form">
@@ -82,17 +118,17 @@ const UserProfile = () => {
 							<label htmlFor="avatar" onClick={() => setIsAvatarClick(true)}>
 								<FaEdit />
 							</label>
+							{isAvatarClick && (
+								<button type="button" className="profile__avatar-btn" onClick={changeAvatar}>
+									<FaCheck />
+								</button>
+							)}
 						</form>
-						{isAvatarClick && (
-							<button className="profile__avatar-btn" onClick={changeAvatar}>
-								<FaCheck />
-							</button>
-						)}
 					</div>
 					<h1>{currentUser.name}</h1>
 					{/* Form to update user details */}
-					<form className="form profile__form">
-						<p className="form__error-message">This is an Error Message</p>
+					<form className="form profile__form" onSubmit={updateUserDetails}>
+						{error && <p className="form__error-message">{error}</p>}
 						<input
 							type="text"
 							placeholder="Full Name"
@@ -110,6 +146,12 @@ const UserProfile = () => {
 							placeholder="Current Password"
 							value={currentPassword}
 							onChange={(e) => setCurrentPassword(e.target.value)}
+						/>
+						<input
+							type="password"
+							placeholder="New Password"
+							value={newPassword}
+							onChange={(e) => setNewPassword(e.target.value)}
 						/>
 						<input
 							type="password"
